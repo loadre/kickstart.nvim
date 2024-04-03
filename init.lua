b765220e45364f64cb9758be9e97139169f49ccf
R Loader mkII <30044659+loadre@users.noreply.github.com>
2024-04-03 13:30:57 -0400

    Added <leader>w for quicker write Removed which-key

--- init.lua
+++ init.lua
@@ -111,6 +111,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- Disable [q:] because I always open cmdline window when trying to quit
 vim.keymap.set('n', 'q:', '<NOP>')
 
+-- More convenient save
+vim.keymap.set('n', '<leader>w', ':w<CR>')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -179,37 +182,37 @@ require('lazy').setup {
     },
   },
 
-  -- NOTE: Plugins can also be configured to run lua code when they are loaded.
+  -- -- NOTE: Plugins can also be configured to run lua code when they are loaded.
+  -- --
+  -- -- This is often very useful to both group configuration, as well as handle
+  -- -- lazy loading plugins that don't need to be loaded immediately at startup.
+  -- --
+  -- -- For example, in the following configuration, we use:
+  -- --  event = 'VimEnter'
+  -- --
+  -- -- which loads which-key before all the UI elements are loaded. Events can be
+  -- -- normal autocommands events (`:help autocmd-events`).
+  -- --
+  -- -- Then, because we use the `config` key, the configuration only runs
+  -- -- after the plugin has been loaded:
+  -- --  config = function() ... end
   --
-  -- This is often very useful to both group configuration, as well as handle
-  -- lazy loading plugins that don't need to be loaded immediately at startup.
+  -- { -- Useful plugin to show you pending keybinds.
+  --   'folke/which-key.nvim',
+  --   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  --   config = function() -- This is the function that runs, AFTER loading
+  --     require('which-key').setup()
   --
-  -- For example, in the following configuration, we use:
-  --  event = 'VimEnter'
-  --
-  -- which loads which-key before all the UI elements are loaded. Events can be
-  -- normal autocommands events (`:help autocmd-events`).
-  --
-  -- Then, because we use the `config` key, the configuration only runs
-  -- after the plugin has been loaded:
-  --  config = function() ... end
-
-  { -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      }
-    end,
-  },
+  --     -- Document existing key chains
+  --     require('which-key').register {
+  --       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+  --       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+  --       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+  --       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+  --       ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  --     }
+  --   end,
+  -- },
 
   -- NOTE: Plugins can specify dependencies.
   --
